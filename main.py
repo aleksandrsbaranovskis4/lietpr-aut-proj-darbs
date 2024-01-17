24c2f27974ee3c17c6583f4d1298b19233c50fee
aleksandrsbaranovskis4 <aleksandrs.baranovskis_4@edu.rtu.lv>
2024-01-17 18:49:31 +0200

    info

--- main.py
+++ main.py
@@ -4,32 +4,31 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from openpyxl import Workbook, load_workbook
-import csv
+from dotenv import load_dotenv
+import os
 import time
-import getpass
 
 def calenderScrape():
     service = Service()
     option = webdriver.ChromeOptions()
     driver = webdriver.Chrome(service=service, options=option)
+    load_dotenv()
     
     url = "https://estudijas.rtu.lv/"
-    while True:
-        driver.get(url)
-        driver.find_element(By.XPATH, "/html/body/div[3]/header[1]/div/div/div[2]/div/form/div[1]/button").click()
-        elem = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[1]/td[2]/form/input")
-        elem.send_keys(input("Enter ORTUS username: "))
-        time.sleep(1)
-        elem = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[2]/td[2]/form/input")
-        elem.send_keys(getpass.getpass("Enter ORTUS password: "))
-        driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[3]/td[2]/input").click()
-        time.sleep(1)
-        if driver.title == "ORTUS (Neveiksmīga autentifikācija)":
-            print("Invalid login info. Try again")
-        else:
-            break
+    driver.get(url)
+    driver.find_element(By.XPATH, "/html/body/div[3]/header[1]/div/div/div[2]/div/form/div[1]/button").click()
+    elem = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[1]/td[2]/form/input")
+    elem.send_keys(os.getenv("USER"))
+    time.sleep(1)
+    elem = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[2]/td[2]/form/input")
+    elem.send_keys(os.getenv("PASS"))
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/div[1]/table/tbody/tr[3]/td[2]/input").click()
+    time.sleep(1)
+    if driver.title == "ORTUS (Neveiksmīga autentifikācija)":
+        print("Invalid login info. Try again")
     
     
+
 
 if __name__ == "__main__":
     calenderScrape()
